83979795a26403b27d01b19e80b5b9b5a7a8776d
bigitonline <72544759+bigitonline@users.noreply.github.com>
2021-04-25 15:36:25 +0300

    Update map.tsx

--- app/src/frontend/map/map.tsx
+++ app/src/frontend/map/map.tsx
@@ -42,7 +42,7 @@ class ColouringMap extends Component<ColouringMapProps, ColouringMapState> {
             theme: 'night',
             lat: 52.5245255,
             lng: -0.1338422,
-            zoom: 61,
+            zoom: 16,
             boundary: undefined,
         };
         this.handleClick = this.handleClick.bind(this);
